b2c387935112d3a42d69a5faba9af5b68195e371
mohammad-mohammadi88 <m.mohammadi8811@gmail.com>
2025-03-31 13:14:45 +0330

    secret key changed

--- app/services/token.js
+++ app/services/token.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 
 const createToken = (data , expiresIn = "10d") => {
     return jwt.sign(
         data,
-        process.env.TOKEN_KEY,
+        process.env.TOKEN_KEY || 'a6d1dfe28604b293d6fc87aeb41b21795f24bd6121f2ebba7717d4700300142ce06e19d20e74e1c6ad423dd1cf9fba1dd75ab0f99f5c49a7daa60cd1f1124b1808c02e2c7379',
         {
             expiresIn, // 60s = 60 seconds - (60m = 60 minutes, 2h = 2 hours, 2d = 2 days)
         }  
